7d9abf0601495bbe6821b81459733b8c7aaff261
Muddassar-Developer <muddassirva@gmail.com>
2026-01-17 22:25:27 +0500

    Test all 5 Operators

--- index.php
+++ index.php
@@ -8,6 +8,83 @@
 <body>
     <div class="container">
     this is my first php website
+    <br>
+  <?php 
+    echo "Hello This is Printed";
+    $variable1 = 5;
+    $variable2 = 2;
+    echo $variable1;
+    echo $variable2;
+//   operators in PHP
+// Arithmetic Operators
+echo "<br>";
+echo "The value of variable1 + variable2 is ";
+echo $variable1 + $variable2;
+echo "<br>";
+echo "The value of variable1 - variable2 is ";
+echo $variable1 - $variable2;
+echo "<br>";
+echo "The value of variable1 * variable2 is ";
+echo $variable1 * $variable2;
+echo "<br>";
+echo "The value of variable1 / variable2 is ";
+echo $variable1 / $variable2;
+echo "<br>";
+
+// Assignment Operators
+$newVar = $variable1;
+// $newVar += 1;
+// $newVar -= 1;
+// $newVar *= 2;
+$newVar /= 2;
+echo "The value of newVar is ";
+echo $newVar;
+echo "<br>";
+// Comparison Operators
+echo "The value of 1==4 is ";
+echo var_dump(1==4);
+echo "<br>";
+echo "The value of 1!=4 is ";
+echo var_dump(1!=4);
+echo "<br>";
+echo "The value of 1>=4 is ";
+echo var_dump(1>=4);
+echo "<br>";
+echo "The value of 1<=4 is ";
+echo var_dump(1<=4);
+echo "<br>";
+// Increment/Decrement Operators
+// echo $variable1++;
+// echo $variable1--;
+// echo --$variable1;
+echo ++$variable1;
+echo "<br>";
+echo $variable1;
+echo "<br>";
+// Logical Operators
+// and (&&)
+// or (||)
+// xor
+// !
+$myVar = (true and true);
+echo var_dump($myVar);
+echo "<br>";
+$myVar = (true and false);
+echo var_dump($myVar);
+echo "<br>";
+$myVar = (false and true);
+echo var_dump($myVar);
+echo "<br>";
+$myVar = (false and false);
+echo var_dump($myVar);
+echo "<br>";
+$myVar = (true xor false);
+echo var_dump($myVar);
+echo "<br>";
+
+
+
+    ?>
 </dive>
 </body>
 </html>
