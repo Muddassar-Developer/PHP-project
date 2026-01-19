2a0363f0bb0fa6fe323068c358300c832e98f76f
Muddassar-Developer <muddassirva@gmail.com>
2026-01-20 04:37:35 +0500

    it is my final project

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$insert = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $con = mysqli_connect("localhost", "root", "", "trip");
+
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $name   = $_POST['name'];
+    $age    = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email  = $_POST['email'];
+    $phone  = $_POST['phone'];
+    $desc   = $_POST['desc'];
+
+    $sql = "INSERT INTO trip (name, age, gender, email, phone, other)
+            VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc')";
+
+    if (mysqli_query($con, $sql)) {
+        $insert = true;
+    } else {
+        die("SQL ERROR: " . mysqli_error($con));
+    }
+
+    mysqli_close($con);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,21 +44,34 @@
 <body>
     <img class="bg" src="bj.jpg" alt="Lahore Uni">
    <div class="container">
+    
+    <form method="post" action="index.php">
     <h1>Well Come to Lahore University Murree Trip Form</h1>
+
     <p>Please enter your details and submit this form to confirm participation in the trip.</p>
-<form action="index.php" method="post"></form>
-<input type="text" name="name" id="name" placeholder="Enter Your Name">
-<input type="text" name="age" id="age" placeholder="Enter Your age">
-<input type="text" name="gender" id="gender" placeholder="Enter Your gender">
-<input type="email" name="email" id="email" placeholder="Enter Your Email">
-<input type="Phone" name="Phone" id="Phone" placeholder="Enter Your Phone Number">
-<textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here."></textarea>
-<button class="btn">Submit</button>
+    
+   <?php 
+   if($insert == true){
+    echo "<p class='submitSmg'>Thans for submitting you form.</p>";
+   }
+    ?>
+<form action="index.php" method="POST">
+    <input type="text" name="name" placeholder="Enter Your Name" required>
+    <input type="number" name="age" placeholder="Enter Your Age" required>
+    <input type="text" name="gender" placeholder="Enter Your Gender" required>
+    <input type="email" name="email" placeholder="Enter Your Email" required>
+    <input type="text" name="phone" placeholder="Enter Your Phone Number" required>
+    <textarea name="desc" placeholder="Enter other information"></textarea>
+    <button class = "btn" type="submit">Submit</button>
+</form>
+
 
 
 
 
    </div> 
 <script src="index.js"></script>
+
+
 </body>
 </html>
